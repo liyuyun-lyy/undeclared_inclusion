e4413dea585f7796388aa0ea194c87a67854b019
本参 <liyuyun.lyy@alibaba-inc.com>
2022-08-17 11:39:07 +0800

    add diff

--- diff.sh
+++ diff.sh
@@ -4,16 +4,22 @@ prefix=$@
 
 cp BUILD1 BUILD
 bazel clean
-bazel --noworkspace_rc build -s :hello --noremote_accept_cached
+#bazel build -s :hello --noremote_accept_cached --execution_log_binary_file=/tmp/${prefix}exec1.log --experimental_remote_grpc_log=$HOME/remote.log
+bazel build -s :hello --execution_log_json_file=/tmp/${prefix}exec1.log --experimental_remote_grpc_log=$HOME/remote.log
 sudo cp bazel-out/k8-fastbuild/bin/_objs/hello/hello.pic.d /tmp/${prefix}hello.pic.d.1
+(cd $HOME/tools_remote && bazel-bin/remote_client --grpc_log $HOME/remote.log printlog > /tmp/${prefix}remote1.log)
 
 cp BUILD2 BUILD
-bazel --noworkspace_rc build -s :hello --noremote_accept_cached
+#bazel build -s :hello --noremote_accept_cached --execution_log_binary_file=/tmp/${prefix}exec2.log --experimental_remote_grpc_log=$HOME/remote.log
+bazel build -s :hello --execution_log_binary_file=/tmp/${prefix}exec2.log --experimental_remote_grpc_log=$HOME/remote.log
 sudo cp bazel-out/k8-fastbuild/bin/_objs/hello/hello.pic.d /tmp/${prefix}hello.pic.d.2
+(cd $HOME/tools_remote && bazel-bin/remote_client --grpc_log $HOME/remote.log printlog > /tmp/${prefix}remote2.log)
 
-bazel clean
-bazel --noworkspace_rc build -s :hello --remote_accept_cached
-sudo cp bazel-out/k8-fastbuild/bin/_objs/hello/hello.pic.d /tmp/${prefix}hello.pic.d.3
+#bazel clean
+#bazel build -s :hello --remote_accept_cached
+#sudo cp bazel-out/k8-fastbuild/bin/_objs/hello/hello.pic.d /tmp/${prefix}hello.pic.d.3
 
 diff /tmp/${prefix}hello.pic.d.1 /tmp/${prefix}hello.pic.d.2
-diff /tmp/${prefix}hello.pic.d.2 /tmp/${prefix}hello.pic.d.3
+diff /tmp/${prefix}exec1.log.txt /tmp/${prefix}exec2.log.txt
+diff /tmp/${prefix}remote1.log /tmp/${prefix}remote2.log
+#diff /tmp/${prefix}hello.pic.d.2 /tmp/${prefix}hello.pic.d.3
