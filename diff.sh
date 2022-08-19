aac90d55385087fe591b8836b87e95098b20f53d
本参 <liyuyun.lyy@alibaba-inc.com>
2022-08-19 10:03:38 +0800

    update diff.sh

--- diff.sh
+++ diff.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 set -x
-prefix=$@
 
-cp /home/admin/undeclared_inclusion/aios/storage/indexlib/indexlib/BUILD1 /home/admin/undeclared_inclusion/aios/storage/indexlib/indexlib/BUILD
+cp $PWD/aios/storage/indexlib/indexlib/BUILD1 $PWD/aios/storage/indexlib/indexlib/BUILD
 bazel clean
-bazel build -s :hello --config=clang --config=debug --config=remote --noremote_accept_cached --remote_executor=grpc://localhost:8980 --experimental_remote_grpc_log=$HOME/remote.log 
+bazel build -s :hello --noremote_accept_cached --remote_executor=grpc://localhost:8980 --experimental_remote_grpc_log=$HOME/remote.log 
 sudo cp bazel-out/k8-fastbuild/bin/_objs/hello/hello.pic.d /tmp/${prefix}hello.pic.d.1
-(cd $HOME/tools_remote && bazel-bin/remote_client --grpc_log $HOME/remote.log printlog > /tmp/${prefix}remote1.log)
+# (cd $HOME/tools_remote && bazel-bin/remote_client --grpc_log $HOME/remote.log printlog > /tmp/remote1.log)
 
-
-cp /home/admin/undeclared_inclusion/aios/storage/indexlib/indexlib/BUILD2 /home/admin/undeclared_inclusion/aios/storage/indexlib/indexlib/BUILD
-bazel build -s :hello --config=clang --config=debug --config=remote --noremote_accept_cached --remote_executor=grpc://localhost:8980 --experimental_remote_grpc_log=$HOME/remote.log
+cp $PWD/aios/storage/indexlib/indexlib/BUILD2 $PWD/aios/storage/indexlib/indexlib/BUILD
+bazel build -s :hello --noremote_accept_cached --remote_executor=grpc://localhost:8980 --experimental_remote_grpc_log=$HOME/remote.log
 sudo cp bazel-out/k8-fastbuild/bin/_objs/hello/hello.pic.d /tmp/${prefix}hello.pic.d.2
-(cd $HOME/tools_remote && bazel-bin/remote_client --grpc_log $HOME/remote.log printlog > /tmp/${prefix}remote2.log)
-
-#bazel clean
-#bazel --noworkspace_rc build -s :hello --remote_accept_cached --remote_executor=grpc://100.69.120.2:8989
-#sudo cp bazel-out/k8-fastbuild/bin/_objs/hello/hello.pic.d /tmp/${prefix}hello.pic.d.3
+#(cd $HOME/tools_remote && bazel-bin/remote_client --grpc_log $HOME/remote.log printlog > /tmp/${prefix}remote2.log)
 
 diff /tmp/${prefix}hello.pic.d.1 /tmp/${prefix}hello.pic.d.2
-#diff /tmp/${prefix}hello.pic.d.2 /tmp/${prefix}hello.pic.d.3
